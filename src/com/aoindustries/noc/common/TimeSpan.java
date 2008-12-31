b68fa1f9e5c82817329eb15508067cb0b8b7093b
Dan Armstrong <dan@aoindustries.com>
2008-12-31 00:09:21 +0000

    Added system time monitoring.

---
+++ src/com/aoindustries/noc/common/TimeSpan.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2008 by AO Industries, Inc.,
+ * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
+ * All rights reserved.
+ */
+package com.aoindustries.noc.common;
+
+import com.aoindustries.sql.SQLUtility;
+import java.io.Serializable;
+
+/**
+ * Encapsulates a time span in milliseconds,
+ * will display the time in client-specific locale.
+ *
+ * @author  AO Industries, Inc.
+ */
+final public class TimeSpan implements Serializable {
+
+    private static long serialVersionUID = 1;
+
+    final private long timeSpan;
+
+    public TimeSpan(long timeSpan) {
+        this.timeSpan = timeSpan;
+    }
+
+    /**
+     * Gets the time span in milliseconds.
+     */
+    public long getTimeSpan() {
+        return timeSpan;
+    }
+
+    @Override
+    public String toString() {
+        return SQLUtility.getMilliDecimal(timeSpan)+" ms";
+    }
+}
