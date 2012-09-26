afdc15e7baa61b21ec41f554412eb2fb17d02c3a
Dan Armstrong <dan@aoindustries.com>
2012-09-26 07:40:24 +0000

    Server ready.

--- src/com/aoindustries/noc/monitor/common/NodeSnapshot.java
+++ src/com/aoindustries/noc/monitor/common/NodeSnapshot.java
@@ -5,10 +5,8 @@
  */
 package com.aoindustries.noc.monitor.common;
 
+import com.aoindustries.util.AoCollections;
 import java.io.Serializable;
-import java.rmi.RemoteException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
@@ -24,50 +22,33 @@ final public class NodeSnapshot implements Serializable {
 
     private static final long serialVersionUID = 1;
 
-    final private NodeSnapshot parent;
-    // TODO: Make this immutable, and return new object on setNode
-    private Node node;
+    final private Node node;
     final private List<NodeSnapshot> children;
     final private AlertLevel alertLevel;
     final private String alertMessage;
     final private boolean allowsChildren;
     final private String id;
     final private String label;
-    // TODO: Make this immutable, and return new object on setNode
-    private UUID uuid;
+    final private UUID uuid;
 
-    /**
-     * Recursively obtains the snapshot of the provided node.
-     */
-    public NodeSnapshot(NodeSnapshot parent, Node node) throws RemoteException {
-        this.parent = parent;
-        this.node = node;
-        List<? extends Node> nodeChildren = node.getChildren();
-        int size = nodeChildren.size();
-        if(size==0) {
-            this.children = Collections.emptyList();
-        } else if(size==1) {
-            this.children = Collections.singletonList(new NodeSnapshot(this, nodeChildren.get(0)));
-        } else {
-            List<NodeSnapshot> modifiableChildren = new ArrayList<NodeSnapshot>(size);
-            for(Node child : nodeChildren) {
-                modifiableChildren.add(new NodeSnapshot(this, child));
-            }
-            this.children = Collections.unmodifiableList(modifiableChildren);
-        }
-        alertLevel = node.getAlertLevel();
-        alertMessage = node.getAlertMessage();
-        allowsChildren = node.getAllowsChildren();
-        id = node.getId();
-        label = node.getLabel();
-        uuid = node.getUuid();
-    }
-
-    /**
-     * Gets the parent of this snapshot or <code>null</code> for none.
-     */
-    public NodeSnapshot getParent() {
-        return parent;
+    public NodeSnapshot(
+        Node node,
+        List<NodeSnapshot> children,
+        AlertLevel alertLevel,
+        String alertMessage,
+        boolean allowsChildren,
+        String id,
+        String label,
+        UUID uuid
+    ) {
+        this.node           = node;
+        this.children       = AoCollections.unmodifiableCopyList(children);
+        this.alertLevel     = alertLevel;
+        this.alertMessage   = alertMessage;
+        this.allowsChildren = allowsChildren;
+        this.id             = id;
+        this.label          = label;
+        this.uuid           = uuid;
     }
 
     /**
@@ -77,15 +58,6 @@ final public class NodeSnapshot implements Serializable {
      */
     public Node getNode() {
         return node;
-    }
-
-    /**
-     * This allows the node to be replaced, this is used when a filter wraps a
-     * node.  Updates both the node and the cached uuid.
-     */
-    public void setNode(Node node) throws RemoteException {
-        this.node = node;
-        this.uuid = node.getUuid();
     }
 
     /**
